aed6db53d52cd9bfa90cbaf613971928c42b297a
CarlosGL090808 <caduvianaribeiro@gmail.com>
2025-07-07 11:30:59 -0300

    Add files via upload

---
+++ agrinho/sketch.js
@@ -0,0 +1,20 @@
+//Load the image.
+function preload() {
+  img = loadImage('pixil-frame-0 (5).png');
+}
+
+function setup() {
+  createCanvas(500, 500);
+
+  background(500);
+
+  // Draw the image.
+  image(img, 0, 0);
+
+  describe('An image of the underside of a rgb(0,255,255) umbrella with a gridded ceiling above.');
+  }
+
+//load the image.
+function preload() {
+  img = loadImage('pixil-frame-0.png')
+}
